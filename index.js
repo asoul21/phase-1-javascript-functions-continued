277f55eb8676706dda070bbd2aba51dd37605fb6
andrew soulinhavong <soulinhavong@gmail.com>
2023-06-04 20:11:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity){
+    if (activity === undefined){
+        activity = 'roller-skate';
+    }
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity){
+    if (activity === undefined){
+        activity = 'go to the office';
+    }
+   return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+    return function (adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
